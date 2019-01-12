a027fae0028ed0292f944d9b14438618f5629782
ertrzyiks <mateusz.derks@gmail.com>
2019-01-12 23:33:19 +0100

    Lazy load images

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -7,7 +7,12 @@ const typography = new Typography({
     'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
     'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     'sans-serif'
-  ]
+  ],
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
+    img: {
+      marginBottom: 0,
+    },
+  })
 })
 
 export default typography
