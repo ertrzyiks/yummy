cbd0d4f891ad243e02281f039463efd283ff4322
asia-t <joanna.derks@gmail.com>
2019-03-01 11:44:52 +0100

    Review changes

--- src/components/menu_close.js
+++ src/components/menu_close.js
@@ -5,7 +5,7 @@ import React from 'react'
  * https://material.io/tools/icons/
  */
 const Icon = (options) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...options}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" {...options}>
     <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
     <path d="M0 0h24v24H0z" fill="none"/>
   </svg>
