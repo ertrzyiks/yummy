e2af9128b8418e3e720ce24ba7c25b1d58549752
ertrzyiks <mateusz.derks@gmail.com>
2019-03-02 16:06:04 +0100

    Fix tag link

--- src/components/stackable_tag/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Tag from '../tag'
-import styles from './stackable_tag.module.sass'
-
-export default function StackableTag({name}) {
-  return <Tag name={name} className={styles.tag} />
-}
