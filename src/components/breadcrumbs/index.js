867529e220e6c70c651becdf64fe4aff9e2d582b
asia-t <joanna.derks@gmail.com>
2019-03-05 09:09:30 +0100

    Review changes

--- src/components/breadcrumbs/index.js
+++ src/components/breadcrumbs/index.js
@@ -5,8 +5,8 @@ import styles from './breadcrumbs.module.sass'
 export default function Breadcrumbs({ subsectionName, subsectionSlug }) {
   return (
     <ol className={styles.breadcrumbs}>
-      <li><Link to="/" className={styles.breadcrumb_link}>Strona Główna</Link></li>
-      <li><Link to={`/${subsectionSlug}`} className={styles.breadcrumb_link}>{subsectionName}</Link></li>
+      <li className={styles.breadcrumb_item}><Link to="/" className={styles.breadcrumb_link}>Strona Główna</Link></li>
+      <li className={styles.breadcrumb_item}><Link to={`/${subsectionSlug}`} className={styles.breadcrumb_link}>{subsectionName}</Link></li>
     </ol>
   )
 }
