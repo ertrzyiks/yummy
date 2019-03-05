e67ec0e34b9e1a593083178fa60e4729d68a3b91
Joanna Derks <joanna.derks@gmail.com>
2019-03-05 15:29:57 +0100

    Fixed pagination links on category and tag pages (#53)
    Fixed pagination links on category and tag pages

--- src/components/paginator/index.js
+++ src/components/paginator/index.js
@@ -5,9 +5,9 @@ import buttonStyles from '../button/button.transparent-noborder.module.sass'
 import ChevronLeft from '../chevron-left'
 import ChevronRight from '../chevron-right'
 
-export default function Paginator ({currentPage, totalPages}) {
-  const nextPagePath = `/page/${currentPage + 1}`
-  const prevPagePath = currentPage === 2 ? `/` : `/page/${currentPage - 1}`
+export default function Paginator ({currentPage, totalPages, subsectionPath = ''}) {
+  const nextPagePath = `${subsectionPath}/page/${currentPage + 1}`
+  const prevPagePath = currentPage === 2 ? `${subsectionPath}/` : `${subsectionPath}/page/${currentPage - 1}`
 
   return <nav className={styles.paginator}>
     {(currentPage > 1) ? <Link to={prevPagePath} className={[buttonStyles.button, styles.prev].join(' ')}><ChevronLeft className={styles.icon} /> Poprzednia</Link> : null}
