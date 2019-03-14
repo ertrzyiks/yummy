9c07a057294fc26c0e2375d01b8072a14dbd301d
ertrzyiks <mateusz.derks@gmail.com>
2019-03-14 20:38:05 +0100

    Add snapshot testing on home page

---
+++ src/utils/theme.js
@@ -0,0 +1,23 @@
+export default {
+  baseFontSize: '16px',
+  bodyFontFamily: [
+    '-apple-system', 'BlinkMacSystemFont',
+    'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
+    'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    'sans-serif'
+  ],
+  overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
+    img: {
+      marginBottom: 0,
+    },
+    ul: {
+      marginBottom: 0,
+    },
+    'p:last-child': {
+      marginBottom: 0,
+    },
+    'li:last-child': {
+      marginBottom: 0,
+    }
+  })
+}
