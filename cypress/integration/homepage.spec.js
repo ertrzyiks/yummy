a5f9401418402876bbad29b14e513584e4a3c55d
ertrzyiks <mateusz.derks@gmail.com>
2019-03-24 16:28:19 +0100

    Add e2e test for search

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -3,9 +3,27 @@ context('Homepage', () => {
     cy.visit('/')
   })
 
-  it('renders the navigation', () => {
+  it('renders properly', () => {
     cy.get('header nav a').should('have.length', 5)
 
-    cy.get('img').should('be.visible')
+    cy.get('section').contains('Desery J').should($card => {
+      expect($card.find('h2')).to.contain('Desery J')
+      expect($card.find('p')).to.contain('Nagłówek')
+      expect($card.find('span')).to.contain('desery')
+      expect($card.find('span')).to.contain('Pokaż przepis')
+      expect($card.find('span')).to.contain('60min')
+    })
+
+    cy.get('a').contains('Następna').should('be.visible')
+    cy.get('span').contains('Strona 1 z 6').should('be.visible')
+  })
+
+  it('allows to search', () => {
+    cy.get('input[type="text"]').type('Koktajl C', {force: true})
+    cy.get('header [role="option"]').contains('Koktajl C').click({force: true})
+
+    cy.location().should(loc => {
+      expect(loc.pathname).to.eq('/koktajle/koktajl-c')
+    })
   })
 })
