9ea7466c9a6e6125cd16b269127af26d3db1bb52
Joanna Derks <joanna.derks@gmail.com>
2019-04-06 21:32:34 +0200

    Show logo in breadcrumbs as a link to the home page (#146)
    * Show logo in breadcrumbs as a link to the home page
    
    * updated the mobile recipe screenshot

--- src/components/breadcrumbs/index.js
+++ src/components/breadcrumbs/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Logo from '../logo_simple'
 import styles from './breadcrumbs.module.sass'
 
 export default function Breadcrumbs({ subsectionName, subsectionSlug }) {
   return (
     <ol className={styles.breadcrumbs}>
-      <li className={styles.breadcrumb_item}><Link to="/" className={styles.breadcrumb_link}>Strona Główna</Link></li>
+      <li className={styles.breadcrumb_item}>
+        <Link to="/" className={styles.breadcrumb_link}>
+          <span className={styles.breadcrumb_homepage_text}>Strona Główna</span>
+          <span className={styles.breadcrumb_homepage_logo}><Logo className={styles.svg_logo} /></span>
+        </Link>
+      </li>
       <li className={styles.breadcrumb_item}><Link to={`/${subsectionSlug}`} className={styles.breadcrumb_link}>{subsectionName}</Link></li>
     </ol>
   )
