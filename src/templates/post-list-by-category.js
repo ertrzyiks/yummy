af96a1e8762678a057a605db7a7e9c8ca98c13b2
Mateusz Derks <mateusz.derks@gmail.com>
2019-05-01 22:19:38 +0200

    Add default og image (#156)

--- src/templates/post-list-by-category.js
+++ src/templates/post-list-by-category.js
@@ -2,27 +2,27 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
 
+import Page from './page'
 import PostListContent from './post-list-content'
 
 export default function PostListByCategoryPage({data, pageContext, location}) {
-  const meta = (
-    <Helmet>
-      <meta name="description" content={`Kolekcja naszych ulubionych przepisów kulinarnych w kategorii: ${pageContext.category}`}></meta>
-    </Helmet>
-  )
-
   const pageInfo = {
     currentPage: pageContext.currentPage,
     totalPages: pageContext.totalPages
   }
 
   return (
-    <PostListContent
-      allRecipesData={data.allRecipe}
-      pageInfo={pageInfo}
-      meta={meta}
-      location={location}
-    />
+    <Page>
+      <Helmet>
+        <meta name="description" content={`Kolekcja naszych ulubionych przepisów kulinarnych w kategorii: ${pageContext.category}`}></meta>
+      </Helmet>
+
+      <PostListContent
+        allRecipesData={data.allRecipe}
+        pageInfo={pageInfo}
+        location={location}
+      />
+    </Page>
   )
 }
 
--- src/templates/post-page.js
+++ src/templates/post-page.js
@@ -8,9 +8,10 @@ import Breadcrumbs from '../components/breadcrumbs'
 import pageStyles from './page.module.sass'
 import postStyles from './post.module.sass'
 import {button as buttonClass} from '../components/button/button.transparent.module.sass'
+import Page from './page'
 import SiteMetadata from '../components/site_metadata'
 
-export default function Template({data}) {
+export default function PostPage({data}) {
   const { recipe } = data
 
   const Tags = (recipe.tags || []).map(tag => {
@@ -18,52 +19,53 @@ export default function Template({data}) {
     return <Link to={'/tag/' + slug} key={tag} className={[buttonClass, postStyles.post_tag].join(' ')}>{tag}</Link>
   })
 
-  return <div className={pageStyles.layout}>
-    <SiteMetadata render={ ({siteUrl}) =>
-      <Helmet>
-        <title>{recipe.html_title}</title>
-        <meta name="description" content={recipe.html_description}/>
-        <meta property="og:image" content={siteUrl + recipe.featured_image.childImageSharp.fluid.src} />
-      </Helmet>
-    } />
+  return <Page>
+    <div className={pageStyles.layout}>
+      <SiteMetadata render={({siteUrl}) =>
+        <Helmet>
+          <title>{recipe.html_title}</title>
+          <meta name="description" content={recipe.html_description}/>
+          <meta property="og:image" content={siteUrl + recipe.featured_image.childImageSharp.fluid.src} />
+        </Helmet>
+      } />
 
-    <section className={pageStyles.main}>
-      <article className={postStyles.post}>
-        <div className={postStyles.post_intro}>
-          <div className={postStyles.post_preamble}>
-            <Breadcrumbs subsectionName={recipe.category.name} subsectionSlug={recipe.category.slug}/>
-            <TimeToPrepare>{recipe.required_time}</TimeToPrepare>
+      <section className={pageStyles.main}>
+        <article className={postStyles.post}>
+          <div className={postStyles.post_intro}>
+            <div className={postStyles.post_preamble}>
+              <Breadcrumbs subsectionName={recipe.category.name} subsectionSlug={recipe.category.slug}/>
+              <TimeToPrepare>{recipe.required_time}</TimeToPrepare>
+            </div>
+
+            <h1 className={postStyles.post_title}>{recipe.name}</h1>
+
+            <div className={postStyles.post_headline} dangerouslySetInnerHTML={{ __html: recipe.headline.childMarkdownRemark.html }}></div>
+
+            <div className={postStyles.post_tags}>
+              {Tags}
+            </div>
           </div>
 
-          <h1 className={postStyles.post_title}>{recipe.name}</h1>
+          <Img
+            fluid={recipe.featured_image.childImageSharp.fluid}
+            alt={'Photography of the food from the recipe.'}
+            className={postStyles.cover_image}
+          />
 
-          <div className={postStyles.post_headline} dangerouslySetInnerHTML={{ __html: recipe.headline.childMarkdownRemark.html }}></div>
-
-          <div className={postStyles.post_tags}>
-            {Tags}
+          <div className={postStyles.post_body}>
+            <div
+              className={postStyles.post_ingredients}
+              dangerouslySetInnerHTML={{ __html: recipe.ingredients.childMarkdownRemark.html }}
+            />
+            <div
+              className={postStyles.post_directions}
+              dangerouslySetInnerHTML={{ __html: recipe.directions.childMarkdownRemark.html}}
+            />
           </div>
-        </div>
-
-        <Img
-          fluid={recipe.featured_image.childImageSharp.fluid}
-          alt={'Photography of the food from the recipe.'}
-          className={postStyles.cover_image}
-        />
-
-        <div className={postStyles.post_body}>
-          <div
-            className={postStyles.post_ingredients}
-            dangerouslySetInnerHTML={{ __html: recipe.ingredients.childMarkdownRemark.html }}
-          />
-          <div
-            className={postStyles.post_directions}
-            dangerouslySetInnerHTML={{ __html: recipe.directions.childMarkdownRemark.html}}
-          />
-        </div>
-      </article>
-    </section>
-  </div>
-
+        </article>
+      </section>
+    </div>
+  </Page>
 }
 
 export const pageQuery = graphql`
