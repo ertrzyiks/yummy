73f816933b08c3fa92671b9a234c819c218970d9
asia-t <joanna.derks@gmail.com>
2019-05-23 14:01:49 +0200

    Added wait times after clicks in the test to see if it resolves the test failures

--- cypress/integration/tag_page.spec.js
+++ cypress/integration/tag_page.spec.js
@@ -5,7 +5,9 @@ context('Tag page', () => {
 
   it('renders properly', () => {
     cy.get('a').contains('Desery J').click()
+    cy.wait(500)
     cy.get('a').contains('Woda').click()
+    cy.wait(500)
     cy.get('section').contains('a', 'Pokaż przepis').parent().find('a').should('have.length', 9)
   })
 })
