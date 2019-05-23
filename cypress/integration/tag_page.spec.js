72e6a03f4e67b8749ba16ddc743ac7c0f3a03d6c
Mateusz Derks <mateusz.derks@gmail.com>
2019-05-23 14:11:11 +0200

    Fix query for recipe tags (#172)
    * Fix query for recipe tags
    
    * Added wait times after clicks in the test to see if it resolves the test failures

---
+++ cypress/integration/tag_page.spec.js
@@ -0,0 +1,13 @@
+context('Tag page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('renders properly', () => {
+    cy.get('a').contains('Desery J').click()
+    cy.wait(500)
+    cy.get('a').contains('Woda').click()
+    cy.wait(500)
+    cy.get('section').contains('a', 'Pokaż przepis').parent().find('a').should('have.length', 9)
+  })
+})
